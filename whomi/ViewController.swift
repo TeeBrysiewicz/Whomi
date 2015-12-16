4a39e61e1fd5e3a8b810a2f65b20a1c8c0dde37d
Tobias Brysiewicz <tbrysiewicz@att.net>
2015-12-16 10:00:36 -0800

    Initial Commit

---
+++ whomi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whomi
+//
+//  Created by Tobias Robert Brysiewicz on 12/16/15.
+//  Copyright © 2015 Tobias Brysiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
